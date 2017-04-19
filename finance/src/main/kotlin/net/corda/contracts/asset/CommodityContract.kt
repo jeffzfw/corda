e3f4095131e52f21c7756f5c04ab8437be14c3d0
Ross Nicoll <ross.nicoll@r3.com>
2017-04-19 17:48:57 +0100

    Update generateIssue in CommodityContract

--- finance/src/main/kotlin/net/corda/contracts/asset/CommodityContract.kt
+++ finance/src/main/kotlin/net/corda/contracts/asset/CommodityContract.kt
@@ -151,13 +151,8 @@ class CommodityContract : OnLedgerAsset<Commodity, CommodityContract.Commands, C
     /**
      * Puts together an issuance transaction for the specified amount that starts out being owned by the given pubkey.
      */
-    fun generateIssue(tx: TransactionBuilder, amount: Amount<Issued<Commodity>>, owner: PublicKey, notary: Party) {
-        check(tx.inputStates().isEmpty())
-        check(tx.outputStates().map { it.data }.sumCashOrNull() == null)
-        val at = amount.token.issuer
-        tx.addOutputState(TransactionState(State(amount, owner), notary))
-        tx.addCommand(generateIssueCommand(), at.party.owningKey)
-    }
+    fun generateIssue(tx: TransactionBuilder, amount: Amount<Issued<Commodity>>, owner: PublicKey, notary: Party)
+            = FungibleAsset.generateIssue(tx, TransactionState(State(amount, owner), notary), generateIssueCommand())
 
 
     override fun deriveState(txState: TransactionState<State>, amount: Amount<Issued<Commodity>>, owner: PublicKey)
